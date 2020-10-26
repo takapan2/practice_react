6a296a1a94a67a66c9bcb94455ae5ca6594894d6
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2020-10-26 14:38:50 +0900

    list3-20

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Rect from "./Rect";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  msgStyle = {
+    fontSize: "24pt",
+    color: "#900",
+    margin: "20px 0",
+    padding: "5px",
+    borderBottom: "2px solid #900",
+  };
+  btnStyle = {
+    fontSize: "20pt",
+    padding: "0 20px",
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      msg: "count start!",
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: "count:" + state.counter,
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+        <p style={this.msgStyle}>{this.state.msg}</p>
+        <button style={this.btnStyle} onClick={this.doAction}>
+          Click
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
