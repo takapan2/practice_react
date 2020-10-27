d7ac727c004428b7054eea3d4fefd9321d653731
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2020-10-27 16:57:56 +0900

    list4-1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+let state_value = {
+  counter: 0,
+  message: "COUNTER",
+};
+
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT",
+      };
+    case "DECREMENT":
+      return {
+        counter: statecouner - 1,
+        message: "DECREMENT",
+      };
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
