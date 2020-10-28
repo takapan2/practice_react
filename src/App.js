6c1804307acd558fb8ca52576c3e593f350225e1
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2020-10-28 18:00:37 +0900

    list4-4

--- src/App.js
+++ src/App.js
@@ -1,70 +1,39 @@
 import { Component } from "react";
-import "./App.css";
-import * as React from "react";
 import { connect } from "react-redux";
+import "./App.css";
+import Memo from "./memo/Memo";
+import Memo from ".memo/FindForm";
+import Memo from "./memo/DelForm";
+import * as React from "react";
 
-function mappingState(state) {
-  return state;
-}
 class App extends Component {
+  td = {
+    width: "250px",
+  };
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}>
+                <FindForm />
+              </td>
+              <td style={this.td}>
+                <DelForm />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
-App = connect()(App);
-
-class Message extends Component {
-  style = {
-    fontSize: "20pt",
-    padding: "20px 5px",
-  };
-  render() {
-    return (
-      <p style={this.style}>
-        {this.props.message}:{this.props.counter}
-      </p>
-    );
-  }
-}
-Message = connect(mappingState)(Message);
-
-class Button extends Component {
-  style = {
-    fontSize: "16pt",
-    padding: "5px 10px",
-  };
-  constructor(props) {
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  doAction(e) {
-    if (e.shiftKey) {
-      this.props.dispatch({ type: "DECREMENT" });
-    } else {
-      this.props.dispatch({ type: "INCREMENT" });
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style} onClick={this.doAction}>
-        click
-      </button>
-    );
-  }
-}
-
-Button = connect()(Button);
-
-export default App;
+export default connect()(App);
