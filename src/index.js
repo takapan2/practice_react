8463a2c3d88d2f343620bf9913216ec4a095634f
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2020-10-29 16:14:02 +0900

    list4-13

--- src/index.js
+++ src/index.js
@@ -1,50 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App";
-// import MemoStore from "./memo/Store";
+import MemoStore, { memoReducer } from "./memo/Store";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-
-let state_value = {
-  counter: 0,
-  message: "COUNTER",
-};
-
-function counter(state = state_value, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-        message: "INCREMENT",
-      };
-    case "DECREMENT":
-      return {
-        counter: state.counter - 1,
-        message: "DECREMENT",
-      };
-
-    case "RESET":
-      return {
-        counter: 0,
-        message: "RESET",
-      };
-    default:
-      return state;
-  }
-}
 
 const persistConfig = {
-  key: "root",
+  key: "memo",
   storage,
 };
 
-const tersistReducer = persistReducer(persistConfig, counter);
-let store = createStore(tersistReducer);
+const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+let store = createStore(persistedReducer);
 let pstore = persistStore(store);
 
 ReactDOM.render(
